d3071a5813f66cc36dc7c0eb9351f442957f5fc0
talbeja <talbeja05@gmail.com>
2022-06-29 11:27:59 +0300

    loan popup

--- userui/src/user/components/loanpopup/LoanPopUpController.java
+++ userui/src/user/components/loanpopup/LoanPopUpController.java
@@ -6,11 +6,9 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
-
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 import loan.Loan;
-import user.utils.HttpClientUtil;
 
 import java.net.URL;
 
@@ -40,38 +38,28 @@ public class LoanPopUpController {
     private Label totalPaidLabel;
 
     @FXML
-    void initialize() {
-        fillLoanInfo();
+    void initialize() { }
 
-    }
-
-    private void fillLoanInfo() {
-        loanIDLabel.textProperty().setValue("Loan ID: " + thisLoan.getId());
-        loanerLabel.textProperty().setValue("Loaner: " + thisLoan.getOwner().getName());
-        statusLabel.textProperty().setValue("Status: " + thisLoan.getStatus().name());
-        categoryLabel.textProperty().setValue("Category: " + thisLoan.getLoanCategory());
-        capitalLabel.textProperty().setValue("Capital: " + thisLoan.getLoan());
+    public void fillLoanInfo() {
+        loanIDLabel.setText("Loan ID: " + thisLoan.getId());
+        loanerLabel.setText("Loaner: " + thisLoan.getOwner().getName());
+        statusLabel.setText("Status: " + thisLoan.getStatus().name());
+        categoryLabel.setText("Category: " + thisLoan.getLoanCategory());
+        capitalLabel.setText("Capital: " + thisLoan.getLoan());
     }
 
     public void popUp(Loan loan) throws Exception{
         this.thisLoan = loan;
-        try{
 
-            Stage newStage = new Stage();
-            FXMLLoader fxmlLoader = new FXMLLoader();
-            URL url = getClass().getResource("/user/components/loanpopup/loanPopUp.fxml");
-            fxmlLoader.setLocation(url);
-            ScrollPane root = fxmlLoader.load(url.openStream());
-            Scene scene =new Scene(root, 400 , 200);
-            newStage.setResizable(false);
-            newStage.setScene(scene);
-            newStage.show();
-
-
-        }catch (Exception e){
-            System.out.println(e);
-        }
-
+        Stage newStage = new Stage();
+        FXMLLoader fxmlLoader = new FXMLLoader();
+        URL url = getClass().getResource("/user/components/loanpopup/loanPopUp.fxml");
+        fxmlLoader.setLocation(url);
+        ScrollPane root = fxmlLoader.load(url.openStream());
+        Scene scene =new Scene(root, 400 , 200);
+        newStage.setResizable(false);
+        newStage.setScene(scene);
+        newStage.show();
 
         //newStage.getIcons().add(new Image("resources/coins.png"));
 
