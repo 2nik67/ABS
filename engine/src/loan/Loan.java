0b1fbce482d31af8e47efeac7a0d55d0a4afbd89
natantunik <2niknatan@gmail.com>
2022-07-12 19:34:33 +0300

    Servlets update

--- engine/src/loan/Loan.java
+++ engine/src/loan/Loan.java
@@ -91,6 +91,7 @@ public class Loan {
             status=Status.ACTIVE;
             activeYaz=Yaz.getYaz();
             owner.loadMoney(loan, owner.getMoney());
+            shouldAutoPay = true;
 
         }
         else{
